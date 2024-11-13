e1cbbe6f56c08f0a762810d00b30bf77e6509334
Kevin LEVRON <kevin.levron@gmail.com>
2024-11-13 12:15:57 +0100

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'  
 
 /**
  * @see https://vitejs.dev/config/
  */
 export default defineConfig({  
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, 'index.html'),
+        test2: resolve(import.meta.dirname, 'test2.html')
+      }
+    }
+  },
   resolve: {
     alias: {
       'three/examples/jsm': 'three/examples/jsm',
